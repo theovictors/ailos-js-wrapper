27f6b01214cd5d75d258ec554834933b296ac282
theovictors <theo_victor@live.com>
2018-11-05 02:30:36 -0200

    Adjust auth structure

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,17 @@
-export default function autha() {
+export default function auth() {
 
-    return this.request({ trn:390 }, {
-        CONTA_CORRENTE: this.contaCorrente,
-        COOPERATIVA: 1,
-        TITULARIDADE: 1,
-        FRASE_SECRETA: this.fraseSecreta,
-        SENHA: this.senha.match(/.{1}/g).join('.')
-      });
-  }
-  
+  return this.request(390, {
+    CONTA_CORRENTE: this.contaCorrente,
+    COOPERATIVA: 1,
+    TITULARIDADE: 1,
+    FRASE_SECRETA: this.fraseSecreta,
+    SENHA: this.senha.match(/.{1}/g).join('.')
+  }).then(response => {
+    if(typeof response.data.OUTPUT_DATA.TOKEN !== 'undefined') {
+      return Promise.resolve(response.data.OUTPUT_DATA.TOKEN);
+    }
+
+    return Promise.reject(new Error('Invalid token'));
+  });
+
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import auth from './auth';
+
 import API_URL from './config';
 import validateAilosResponse from './utils';
-import config from './config';
 
 export default class AilosWrapper {
   constructor(options) {
@@ -8,22 +9,8 @@ export default class AilosWrapper {
     this.fraseSecreta = options.fraseSecreta;
     this.senha = options.senha;
     this.token = '';
-  }
 
-  auth() {
-    return this.request(390, {
-      CONTA_CORRENTE: this.contaCorrente,
-      COOPERATIVA: 1,
-      TITULARIDADE: 1,
-      FRASE_SECRETA: this.fraseSecreta,
-      SENHA: this.senha.match(/.{1}/g).join('.')
-    }).then(response => {
-      if(typeof response.data.OUTPUT_DATA.TOKEN !== 'undefined') {
-        return Promise.resolve(response.data.OUTPUT_DATA.TOKEN);
-      }
-
-      return Promise.reject(new Error('Invalid token'));
-    });
+    this.auth = auth.bind(this);
   }
 
   setToken(token) {
